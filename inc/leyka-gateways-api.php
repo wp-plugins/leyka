c6b804d546426052ef8b895a708a141ee45c9dda
Ahaenor <Ahaenor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-03 09:18:12 +0000

    A security and service fixes.
    git-svn-id: https://plugins.svn.wordpress.org/leyka/trunk@1058934 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/leyka-gateways-api.php
+++ inc/leyka-gateways-api.php
@@ -301,6 +301,9 @@ abstract class Leyka_Gateway {
      */
     public function add_payment_method(Leyka_Payment_Method $pm, $replace_if_exists = false) {
 
+        if($pm->gateway_id != $this->_id)
+            return false;
+
         if(empty($this->_payment_methods[$pm->id]) || !!$replace_if_exists) {
             $this->_payment_methods[$pm->id] = $pm;
             return true;
@@ -382,7 +385,7 @@ abstract class Leyka_Payment_Method {
             case 'is_active': $param = $this->_active; break;
             case 'label':
             case 'title':
-            case 'name': $param = $this->_label; break;
+            case 'name': $param = apply_filters('leyka_get_pm_label', $this->_label, $this); break;
             case 'label_backend':
             case 'title_backend':
             case 'name_backend': $param = $this->_label_backend ? $this->_label_backend : $this->_label;
